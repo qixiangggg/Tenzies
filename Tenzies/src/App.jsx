798bb402545f360034f3a9cd4e4a15bb1453c42d
DesmondChong <91426701+qixiangggg@users.noreply.github.com>
2025-06-20 19:18:56 +0800

    - Setup - Initialize the layout - Create 10 die on the page - Make the die number random - refactor the die components to map array instead of hardcoded

---
+++ Tenzies/src/App.jsx
@@ -0,0 +1,23 @@
+import Die from "./components/Die"
+import { useState } from "react"
+
+export default function App(){
+  const [diceNumbers, setDiceNumbers] = useState(generateAllNewDice())
+
+  function generateAllNewDice(){
+        const randomNumbers = []
+
+        for (let i = 0; i < 10; i++){
+            randomNumbers.push(Math.floor(Math.random() * 6) + 1);
+        }
+        return randomNumbers
+    }
+    
+  return (
+    <main>
+      <div className="dice-container">
+        {diceNumbers.map((diceNumber) => <Die value={diceNumber}/>)}
+      </div>
+    </main>
+  )
+}
